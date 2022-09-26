185419791e6f11d949072f932200b6676efa65d2
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-09-26 19:08:59 +0600

    Overall Project Overview

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,20 +1,31 @@
-document.getElementById('withdraw_btn').addEventListener('click', function(){
+document.getElementById('withdraw_btn').addEventListener('click', function () {
     const withdrawButton = document.getElementById('withdraw_box')
     const withdrawFuildElementString = withdrawButton.value
     const withdrawFuildElementNumber = parseFloat(withdrawFuildElementString);
-
+    // console.log(withdrawFuildElementNumber);
     const withdrawFuild = document.getElementById('widthdraw_total');
     const withdrawFuildString = withdrawFuild.innerText;
     const withdrawFuildNumber = parseFloat(withdrawFuildString)
     const newWithdrawFuild = withdrawFuildElementNumber;
-    withdrawFuild.innerText = newWithdrawFuild + withdrawFuildNumber; 
+    // withdrawFuild.innerText = newWithdrawFuild + withdrawFuildNumber; 
+
 
 
     const totalBlance = document.getElementById('blance_total');
     const totalBlanceString = totalBlance.innerText;
     const totalBlanceNumber = parseFloat(totalBlanceString);
-    const totalCurrentBlance = totalBlanceNumber -  withdrawFuildElementNumber;
-    totalBlance.innerText = totalCurrentBlance
+    const totalCurrentBlance = totalBlanceNumber - withdrawFuildElementNumber;
+    withdrawButton.value = '';
 
-        withdrawButton.value = '';
+    if (totalBlanceNumber < withdrawFuildElementNumber) {
+        alert('baper atto tk nai');
+        return;
+    }
+    withdrawFuild.innerText = newWithdrawFuild + withdrawFuildNumber; //akane dile return laj korbe tai akane deuya ...!
+
+    totalBlance.innerText = totalCurrentBlance;
+
+
+
+
 })
